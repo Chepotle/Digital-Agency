ac1dcce98980f39c1d16bdbc57e3c3b39af37675
Alex Agaev <alex34agaev@gmail.com>
2021-07-22 18:35:03 +0300

    валидация формы

--- js/form.js
+++ js/form.js
@@ -4,11 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
-
     async function formSend(e) {
         e.preventDefault();
 
         let error = formValidate(form);
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            let formBlock = document.querySelector('.form');
+            formBlock.classList.add('_sending');
+
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                formBlock.classList.remove('_sending');
+            } else {
+                alert('Error');
+                formBlock.classList.remove('_sending');
+            }
+        } else {
+            alert('Fill in required fields')
+        }
     }
 
 
@@ -18,22 +39,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
         for (const input of formReq) {
             formRemoveError(input);
-        }
 
-        if (input.classList.contains('_email')) {
-            if (emailTest(input)) {
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
                 formAddError(input);
                 error++;
-            }
-        } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
-            formAddError(input);
-            error++;
-        } else {
-            if (input.value === '') {
-                formAddError(input);
-                erorr++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
             }
         }
+        return error
     }
 
     function formAddError(input) {
